309526637fa3e86aa8386d46edba77480c502975
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-05-08 17:03:26 -0400

    ✨ initialize cat controller and animation

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/cat.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,5 +37,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget buildAnimation() {}
+  Widget buildAnimation() {
+    return Cat();
+  }
 }
