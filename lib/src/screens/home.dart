53d45171013f55dcd1f90b18e4cd0cb58cf027ad
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-05-08 20:42:35 -0400

    ✨ animate cat inside box

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -16,10 +16,10 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -35.0, end: -120.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
@@ -34,7 +34,15 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         title: Text('Animation!'),
       ),
       body: GestureDetector(
-        child: buildAnimation(),
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              buildCatAnimation(),
+              buildBox(),
+            ],
+          ),
+        ),
         onTap: onTap,
       ),
     );
@@ -48,16 +56,26 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     }
   }
 
-  Widget buildAnimation() {
+  Widget buildCatAnimation() {
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (context, child) {
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value),
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
         );
       },
       child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      width: 200,
+      height: 250,
+      color: Colors.brown,
     );
   }
 }
